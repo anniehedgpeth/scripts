b9532861f083afa6702f05b879fe6def395bb739
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-06-29 10:27:35 -0500

    commenting out the manual steps

--- automateinstall.sh
+++ automateinstall.sh
@@ -17,17 +17,16 @@ SUPERMARKET_FQDN=$9
 rpm -Uvh $AUTOMATE_DOWNLOAD_URL
 
 # Do a preflight-check
-automate-ctl prelight-check
-chfs -a size=80G /var
+automate-ctl preflight-check
 
 # Configure Automate server; ensure that the delivery.license is on the Chef Server
-automate-ctl setup --license "/home/$ADMIN_USERNAME/$AUTOMATE_LICENSE" \ 
-                   --key "$AUTOMATE_CHEF_USER_KEY" \ 
-                   --server-url https://$CHEF_SERVER_FQDN/organizations/$ORGANIZATION_SHORT_NAME \ 
-                   --fqdn $AUTOMATE_SERVER_FQDN \ 
-                   --enterprise $ENTERPRISE_NAME \
-                   --supermarket-fqdn $SUPERMARKET_FQDN \
-                   --configure
+# automate-ctl setup --license "/home/$ADMIN_USERNAME/$AUTOMATE_LICENSE" \ 
+#                    --key "$AUTOMATE_CHEF_USER_KEY" \ 
+#                    --server-url https://$CHEF_SERVER_FQDN/organizations/$ORGANIZATION_SHORT_NAME \ 
+#                    --fqdn $AUTOMATE_SERVER_FQDN \ 
+#                    --enterprise $ENTERPRISE_NAME \
+#                    --supermarket-fqdn $SUPERMARKET_FQDN \
+#                    --configure
 
 # Reconfigure
-automate-ctl reconfigure
+# automate-ctl reconfigure
